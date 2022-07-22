a7368d4fbf5776b164da8853f771e58cf0286eb4
dudu <whqtkf12@naver.com>
2022-07-22 20:59:36 +0900

    feat: APIProvider 구현

---
+++ DoneList/Network/APIProvider/APIProvider.swift
@@ -0,0 +1,27 @@
+//
+//  APIProvider.swift
+//  DoneList
+//
+//  Created by dudu on 2022/07/22.
+//
+
+import Foundation
+import Combine
+
+final class APIProvider {
+    private let urlSession: URLSession
+    
+    init(urlSession: URLSession = .shared) {
+        self.urlSession = urlSession
+    }
+    
+    func request(_ api: EndPoint) -> AnyPublisher<Data, URLError> {
+        guard let urlRequest = api.makeURLRequest() else {
+            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+        }
+        
+        return urlSession.dataTaskPublisher(for: urlRequest)
+            .map(\.data)
+            .eraseToAnyPublisher()
+    }
+}
