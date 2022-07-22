6dd59cee700ebac2b98906d0c291ee4badb78939
dudu <whqtkf12@naver.com>
2022-07-22 19:22:23 +0900

    feat: HttpMethod 열거형 추가

---
+++ DoneList/Network/EndPoint/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  DoneList
+//
+//  Created by 김도연 on 2022/07/22.
+//
+
+import Foundation
+
+enum HttpMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
