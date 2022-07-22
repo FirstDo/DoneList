5f4fdf72e4260fffe34e5028d56aafa98c531ce2
dudu <whqtkf12@naver.com>
2022-07-22 22:55:51 +0900

    feat: AppCoordinator 구현

---
+++ DoneList/Presentation/Coordinator/AppCoordinator.swift
@@ -0,0 +1,44 @@
+//
+//  AppCoordinator.swift
+//  DoneList
+//
+//  Created by dudu on 2022/07/22.
+//
+
+import UIKit
+
+protocol Coordinator: AnyObject {
+    var navigationController: UINavigationController? { get set }
+    var parentCoordinator: Coordinator? { get set }
+    var childCoordinator: [Coordinator] { get set }
+}
+
+extension Coordinator {
+    func removeChild(_ child: Coordinator) {
+        childCoordinator.removeAll { $0 === child }
+    }
+}
+
+final class AppCoordinator: Coordinator {
+    var navigationController: UINavigationController?
+    weak var parentCoordinator: Coordinator?
+    var childCoordinator: [Coordinator] = []
+    
+    private let dependency: AppDIContainer
+    
+    init(navigationController: UINavigationController, dependency: AppDIContainer) {
+        self.navigationController = navigationController
+        self.dependency = dependency
+    }
+    
+    func start() {
+        guard let navigationController = navigationController else { return }
+        
+        let doneSceneDIContainer = dependency.makeDoneSceneDIContainer()
+        let doneSceneCoordinator = doneSceneDIContainer.makeDoneSceneCoordinator(navigationController)
+        doneSceneCoordinator.parentCoordinator = self
+        childCoordinator.append(doneSceneCoordinator)
+        
+        doneSceneCoordinator.showDoneList()
+    }
+}
