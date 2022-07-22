900cc96cc5c516f18d9ab7975d6bf65c716a2e80
dudu <whqtkf12@naver.com>
2022-07-23 01:49:52 +0900

    feat: mainView를 DoneListViewController에 추가

--- DoneList/Presentation/Scenes/DoneList/View/DoneListViewController.swift
+++ DoneList/Presentation/Scenes/DoneList/View/DoneListViewController.swift
@@ -8,14 +8,18 @@
 import UIKit
 import Combine
 
+import SnapKit
+
 final class DoneListViewController: UIViewController {
     
     weak var coordinator: DoneSceneCoordiantor?
     private let viewModel: DoneListViewModelType
+    private var mainView: DoneListView
     private var cancelBag = Set<AnyCancellable>()
 
     init(_ viewModel: DoneListViewModelType) {
         self.viewModel = viewModel
+        self.mainView = DoneListView()
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -25,5 +29,19 @@ final class DoneListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setup()
+    }
+    
+    private func setup() {
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        view.addSubview(mainView)
+        
+        mainView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
