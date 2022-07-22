19e9499548b15218f25278420c7ce9e2f122d715
dudu <whqtkf12@naver.com>
2022-07-22 22:10:39 +0900

    refactor: StorageType이 에러를 처리할 수 있게, Publisher를 방출하도록 수정

--- DoneList/Data/Storage/StorageType.swift
+++ DoneList/Data/Storage/StorageType.swift
@@ -6,33 +6,47 @@
 //
 
 import Combine
+import Foundation
+
+enum StorageError: LocalizedError {
+    case createFail
+    case readFail
+    case updateFail
+    case deleteFail
+}
 
 protocol StorageType {
-    func create(_ item: Done)
+    func create(_ item: Done) -> Completable<StorageError>
     func read() -> AnyPublisher<[Done], Never>
-    func update(_ item: Done)
-    func delete(_ item: Done)
+    func update(_ item: Done) -> Completable<StorageError>
+    func delete(_ item: Done) -> Completable<StorageError>
 }
 
 final class MemoryStorage: StorageType {
     
     @Published private var items = [Done]()
     
-    func create(_ item: Done) {
+    func create(_ item: Done) -> Completable<StorageError> {
         items.append(item)
+        
+        return Empty().eraseToAnyPublisher()
     }
     
     func read() -> AnyPublisher<[Done], Never> {
         return $items.eraseToAnyPublisher()
     }
     
-    func update(_ item: Done) {
+    func update(_ item: Done) -> Completable<StorageError> {
         if let index = items.firstIndex(where: {$0.id == item.id }) {
             items[index] = item
         }
+        
+        return Empty().eraseToAnyPublisher()
     }
     
-    func delete(_ item: Done) {
+    func delete(_ item: Done) -> Completable<StorageError> {
         items.removeAll { $0.id == item.id }
+        
+        return Empty().eraseToAnyPublisher()
     }
 }
