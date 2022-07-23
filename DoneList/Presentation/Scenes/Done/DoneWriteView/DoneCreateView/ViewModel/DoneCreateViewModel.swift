9c6c7076a9fb00a38e76fef85b4a234e4327bc7e
dudu <whqtkf12@naver.com>
2022-07-23 18:27:10 +0900

    feat: DoneCreateViewModel 구현

---
+++ DoneList/Presentation/Scenes/Done/DoneWriteView/DoneCreateView/ViewModel/DoneCreateViewModel.swift
@@ -0,0 +1,51 @@
+//
+//  DoneCreateViewModel.swift
+//  DoneList
+//
+//  Created by dudu on 2022/07/23.
+//
+
+import Foundation
+import Combine
+
+protocol DoneViewModelInput {
+    func didTapCreateButton(_ imageName: String?, _ taskName: String?)
+    func didTapCell(_ imageName: String)
+}
+
+protocol DoneViewModelOutput {
+    var doneImageName: PassthroughSubject<String, Never> { get }
+    var dismissView: PassthroughSubject<Void, Never> { get }
+}
+
+protocol DoneViewModelType: DoneViewModelInput, DoneViewModelOutput {}
+
+final class DoneViewModel: DoneViewModelType {
+   
+    private let doneUseCase: DoneUseCaseType
+    private var cancelBag = Set<AnyCancellable>()
+    
+    // MARK: - Output
+    
+    var doneImageName = PassthroughSubject<String, Never>()
+    let dismissView = PassthroughSubject<Void, Never>()
+    
+    init(doneUseCase: DoneUseCaseType) {
+        self.doneUseCase = doneUseCase
+    }
+}
+
+// MARK: - Input
+
+extension DoneViewModel {
+    func didTapCreateButton(_ imageName: String?, _ taskName: String?) {
+        guard let imageName = imageName, let taskName = taskName else { return }
+        
+        _ = doneUseCase.createNewItem(Done(createdAt: Date.now, taskName: taskName, imageName: imageName))
+        dismissView.send()
+    }
+    
+    func didTapCell(_ imageName: String) {
+        doneImageName.send(imageName)
+    }
+}
