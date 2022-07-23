d5610d81e30e398a558ffd00a3c8441b3c48bce9
dudu <whqtkf12@naver.com>
2022-07-23 16:43:44 +0900

    refactor: Done Entity에 createdAt 프로퍼티 추가

--- DoneList/Domain/Entity/Done.swift
+++ DoneList/Domain/Entity/Done.swift
@@ -9,11 +9,13 @@ import Foundation
 
 struct Done: Hashable {
     let id: String
+    let createdAt: Date
     let taskName: String
     let imageName: String
     
-    init(id: String = UUID().uuidString, taskName: String, imageName: String) {
+    init(id: String = UUID().uuidString, createdAt: Date, taskName: String, imageName: String) {
         self.id = id
+        self.createdAt = createdAt
         self.taskName = taskName
         self.imageName = imageName
     }
@@ -22,12 +24,20 @@ struct Done: Hashable {
 #if DEBUG
 extension Done {
     static func dummy() -> [Done] {
+        let oneDay: TimeInterval = 86_400
+        
+        let today = Date.now
+        let yesterDay = today.addingTimeInterval(-oneDay)
+        let tomorrow = today.addingTimeInterval(oneDay)
+        let oneMonthBefore = today.addingTimeInterval(-oneDay * 30)
+        let oneMonthAfter = today.addingTimeInterval(oneDay * 30)
+        
         return [
-            Done(taskName: "iOS 공부를 했다", imageName: "iphone.homebutton"),
-            Done(taskName: "유튜브를 봤다", imageName: "play.rectangle.fill"),
-            Done(taskName: "운동을 했다", imageName: "heart.fill"),
-            Done(taskName: "게임을 했다!!", imageName: "gamecontroller.fill"),
-            Done(taskName: "회사에 갔다!! 그리고 매우매우 매우 매우 긴 레이블입니다", imageName: "building.2"),
+            Done(createdAt: today, taskName: "iOS 공부를 했다", imageName: "iphone.homebutton"),
+            Done(createdAt: yesterDay, taskName: "유튜브를 봤다", imageName: "play.rectangle.fill"),
+            Done(createdAt: tomorrow, taskName: "운동을 했다", imageName: "heart.fill"),
+            Done(createdAt: oneMonthBefore, taskName: "게임을 했다!!", imageName: "gamecontroller.fill"),
+            Done(createdAt: oneMonthAfter, taskName: "회사에 갔다!! 그리고 매우매우 매우 매우 긴 레이블입니다", imageName: "building.2"),
         ]
     }
 }
