e2638c4a353ab78fbdeba1e287a8d44a54cce15a
dudu <whqtkf12@naver.com>
2022-07-23 19:16:59 +0900

    feat: Category Entity에 dummy값 추가

--- DoneList/Domain/Entity/Category.swift
+++ DoneList/Domain/Entity/Category.swift
@@ -7,4 +7,15 @@
 
 struct Category: Hashable {
     let name: String
+    
+    static let all: [Category] = [
+        Category(name: "books.vertical.fill"), // 독서
+        Category(name: "moon.zzz"),
+        Category(name: "cross.circle.fill"),
+        Category(name: "paintpalette"),
+        Category(name: "fork.knife"),
+        Category(name: "gift"),
+        Category(name: "message.circle"),
+        Category(name: "rectangle.and.pencil.and.ellipsis"),
+    ]
 }
