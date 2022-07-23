99692a830ac151cf59a243e9d38c58c852a468b7
dudu <whqtkf12@naver.com>
2022-07-24 02:09:08 +0900

    feat: Category에 Image의 tintColor를 설정해줄 color 프로퍼티 추가

--- DoneList/Domain/Entity/Category.swift
+++ DoneList/Domain/Entity/Category.swift
@@ -5,17 +5,22 @@
 //  Created by dudu on 2022/07/23.
 //
 
+import UIKit
+
 struct Category: Hashable {
     let name: String
+    let color: UIColor
+    
+    static let empty = Category(name: "plus", color: .systemGray)
     
     static let all: [Category] = [
-        Category(name: "books.vertical.fill"), // 독서
-        Category(name: "moon.zzz"),
-        Category(name: "cross.circle.fill"),
-        Category(name: "paintpalette"),
-        Category(name: "fork.knife"),
-        Category(name: "gift"),
-        Category(name: "message.circle"),
-        Category(name: "rectangle.and.pencil.and.ellipsis"),
+        Category(name: "books.vertical.fill", color: .systemRed), // 독서
+        Category(name: "moon.zzz", color: .systemBlue),
+        Category(name: "cross.circle.fill", color: .systemGreen),
+        Category(name: "paintpalette", color: .systemYellow),
+        Category(name: "fork.knife", color: .systemTeal),
+        Category(name: "gift", color: .systemCyan),
+        Category(name: "message.circle", color: .systemMint),
+        Category(name: "rectangle.and.pencil.and.ellipsis", color: .systemBrown),
     ]
 }
