1471dc7c987eadb84303e825a2ff0184939c6be9
dudu <whqtkf12@naver.com>
2022-07-27 01:55:49 +0900

    feat: DoneSettingViewModel 생성

---
+++ DoneList/Presentation/Scenes/DoneSettingScene/ViewModel/DoneSettingViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  DoneSettingViewModel.swift
+//  DoneList
+//
+//  Created by dudu on 2022/07/27.
+//
+
+import Combine
+
+protocol DoneSettingViewModelInput {
+    
+}
+
+protocol DoneSettingViewModelOutput {
+    
+}
+
+protocol DoneSettingViewModelType: DoneSettingViewModelInput, DoneSettingViewModelOutput { }
+
+final class DoneSettingViewModel: DoneSettingViewModelType {
+    
+}
