d81d8762fb94248bc7be0ab7306592542df063cb
dudu <whqtkf12@naver.com>
2022-07-29 01:48:19 +0900

    feat: OpenSoureViewModel 구현

---
+++ DoneList/Presentation/Scenes/DoneSettingScene/OpenSourceView/ViewModel/OpenSoureViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  OpenSoureViewModel.swift
+//  DoneList
+//
+//  Created by dudu on 2022/07/29.
+//
+
+protocol OpenSourceViewModelInput {
+    
+}
+
+protocol OpenSourceViewModelOutput {
+    var item: OpenSource { get }
+}
+
+protocol OpenSourceViewModelType: OpenSourceViewModelInput, OpenSourceViewModelOutput {}
+
+final class OpenSoureViewModel: OpenSourceViewModelType {
+    let item: OpenSource
+    
+    init(item: OpenSource) {
+        self.item = item
+    }
+}
