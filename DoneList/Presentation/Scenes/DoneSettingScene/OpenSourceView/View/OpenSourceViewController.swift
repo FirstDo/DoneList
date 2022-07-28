05a56b2b97583284e59ef6c79cee9598dff5e8dc
dudu <whqtkf12@naver.com>
2022-07-29 02:24:24 +0900

    feat: OpenSoureViewController 구현

---
+++ DoneList/Presentation/Scenes/DoneSettingScene/OpenSourceView/View/OpenSourceViewController.swift
@@ -0,0 +1,50 @@
+//
+//  OpenSourceViewController.swift
+//  DoneList
+//
+//  Created by dudu on 2022/07/29.
+//
+
+import UIKit
+
+final class OpenSourceViewController: UIViewController {
+    
+    private let openSourceContentTextView: UITextView = {
+        let textView = UITextView()
+        
+        return textView
+    }()
+    
+    private let viewModel: OpenSourceViewModelType
+    
+    init(_ viewModel: OpenSourceViewModelType) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+        bind()
+    }
+    
+    private func bind() {
+        openSourceContentTextView.text = viewModel.item.content
+    }
+    
+    private func setup() {
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        view.addSubview(openSourceContentTextView)
+        openSourceContentTextView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide).inset(10)
+        }
+    }
+}
