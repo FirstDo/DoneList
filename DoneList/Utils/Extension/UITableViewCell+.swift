790ad5dbd20565ad98457ebe3f7ff737dc47284f
dudu <whqtkf12@naver.com>
2022-08-06 03:43:15 +0900

    feat: UITableViewCell extension 추가
    - cell의 identifier를 관리

---
+++ DoneList/Utils/Extension/UITableViewCell+.swift
@@ -0,0 +1,14 @@
+//
+//  UITableViewCell+.swift
+//  DoneList
+//
+//  Created by dudu on 2022/08/06.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
