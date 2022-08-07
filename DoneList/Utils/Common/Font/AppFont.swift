4e47cb489f16fdbf5ce03698684c93e90110a0c9
dudu <whqtkf12@naver.com>
2022-08-08 02:51:04 +0900

    feat: AppFont 파일 생성
    - 폰트 사이즈를 나타내는 FonSize 열거형 구현
    - 폰트의 종류를 나타내는 AppFont  열거형 구현
    - SwiftUI에서 사용하는 customFont 매서드 구현
    - UIKit에서 사용하는 customFont 매서드 구현
    - Font를 UserDefaults에 저장하는 FontManager 구현

---
+++ DoneList/Utils/Common/Font/AppFont.swift
@@ -0,0 +1,68 @@
+//
+//  AppFont.swift
+//  DoneList
+//
+//  Created by dudu on 2022/08/08.
+//
+
+import UIKit
+import SwiftUI
+
+enum FontSize: CGFloat {
+    case largetTitle = 34
+    case title1 = 28
+    case title2 = 22
+    case title3 = 20
+    case body = 17
+    case callout = 16
+    case footnote = 13
+    case caption1 = 12
+    case caption2 = 11
+}
+
+enum AppFont: String, CaseIterable {
+    case system = "System"
+    case dalseoHealing = "DalseoHealing"
+    case mabinogi = "Mabinogi_Classic_OTF"
+    case chosunCentennial = "Chosun Centennial"
+    
+    var name: String {
+        switch self {
+        case .system:
+            return "시스템 폰트"
+        case .dalseoHealing:
+            return "달서힐링체"
+        case .mabinogi:
+            return "마비옛체"
+        case .chosunCentennial:
+            return "조선100년체"
+        }
+    }
+}
+
+extension UIFont {
+    func customFont(_ appFont: AppFont, _ size: FontSize) -> UIFont {
+        return UIFont(name: appFont.rawValue, size: size.rawValue) ?? .systemFont(ofSize: size.rawValue)
+    }
+}
+
+extension View {
+    func customFont(_ appFont: AppFont, _ size: FontSize) -> some View {
+        if appFont == .system {
+            return self.font(.system(size: size.rawValue))
+        } else {
+            return self.font(.custom(appFont.rawValue, size: size.rawValue))
+        }
+    }
+}
+
+final class FontManager {
+    static func getFontName() -> AppFont {
+        let key = UserDefaults.standard.string(forKey: "font") ?? "System"
+        return AppFont(rawValue: key)!
+    }
+    
+    static func setFont(_ font: AppFont) {
+        UserDefaults.standard.setValue(font.rawValue, forKey: "font")
+    }
+}
