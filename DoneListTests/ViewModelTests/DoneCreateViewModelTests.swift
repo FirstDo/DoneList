5686e67d885a803b790659b4d52ffdfc90f38204
dudu <whqtkf12@naver.com>
2022-08-19 22:26:19 +0900

    test: DoneCreateViewModel 테스트 구현

--- DoneListTests/ViewModelTests/DoneCreateViewModelTests.swift
+++ DoneListTests/ViewModelTests/DoneCreateViewModelTests.swift
@@ -1,5 +1,5 @@
 //
-//  DoneEditViewModelTests.swift
+//  DoneCreateViewModelTests.swift
 //  DoneListTests
 //
 //  Created by dudu on 2022/08/19.
@@ -8,53 +8,49 @@
 @testable import DoneList
 import XCTest
 
-import Combine
-
-class DoneEditViewModelTests: XCTestCase {
+class DoneCreateViewModelTests: XCTestCase {
     
-    var sut: DoneEditViewModelType!
-    var mockDoneUseCase: MockDoneUseCase!
-    let done = Done(createdAt: .now, taskName: "초기값", category: Category(name: "초기카테고리"))
+    var sut: DoneCreateViewModel!
+    var mockUseCase: MockDoneUseCase!
+    let targetDate = Date.now
 
     override func setUpWithError() throws {
-        mockDoneUseCase = MockDoneUseCase()
-        sut = DoneEditViewModel(doneUseCase: mockDoneUseCase, done: done)
+        mockUseCase = MockDoneUseCase()
+        sut = DoneCreateViewModel(doneUseCase: mockUseCase, date: targetDate)
     }
 
     override func tearDownWithError() throws {
-        mockDoneUseCase = nil
+        mockUseCase = nil
         sut = nil
     }
     
-    func test_didTapEditButton을호출했을때_usecase의editItem이호출되어야한다() {
+    func test_didTapCreateButton을호출했을때_doneUseCase의CreateNewItem이호출되어야한다() {
         // given
-        let expectedItem = Done(id: done.id, createdAt: done.createdAt, taskName: sut.taskTitle.value, category: sut.category.value)
         
         // when
-        sut.didTapEditButton()
+        sut.didTapCreateButton()
         
         // then
-        XCTAssertEqual(1, mockDoneUseCase.editItemCallCount)
-        XCTAssertEqual(expectedItem, mockDoneUseCase.item)
+        XCTAssertEqual(mockUseCase.createNewItemCallCount, 1)
     }
     
-    func test_didTapEditButton을호출했을때_dismissView에Void가전달되어야한다() {
+    func test_didTapCreateButton을호출했을때_dismissView에void가전달되어야한다() {
         // given
-        let expectation = XCTestExpectation(description: "dismissView에Void가전달되어야한다")
-        var receiveEvent = false
+        let expectation = XCTestExpectation(description: "void가 전달되어야한다")
+        let expectedValue = true
+        var value = false
         
         // when
         let cancellable = sut.dismissView
-            .print()
-            .sink { empty in
-                receiveEvent = true
+            .sink { _ in
+                value = true
                 expectation.fulfill()
             }
-        sut.didTapEditButton()
+        sut.didTapCreateButton()
         
         // then
-        wait(for: [expectation], timeout: 10)
-        XCTAssertEqual(receiveEvent, true)
+        wait(for: [expectation], timeout: 10.0)
+        XCTAssertEqual(expectedValue, value)
     }
     
     func test_didTapCell을호출했을때_categorySubject에Item이전달되어야한다() {
@@ -68,15 +64,8 @@ class DoneEditViewModelTests: XCTestCase {
         XCTAssertEqual(sut.category.value, category)
     }
     
-    func test_didEditTextField를호출했을때_taskTitle에Text가전달되어야한다() {
-        // given
-        let text = "테스트입니다"
-        
-        // when
-        sut.didEditTextField(text)
-        
-        // then
-        XCTAssertEqual(sut.taskTitle.value, text)
+    func test_didEditTextField를호출했을때_doneTitle에text가전달되어야한다() {
+        // empty
     }
     
     func test_cellItems을호출했을때_모든Category가반환되어야한다() {
@@ -97,11 +86,11 @@ class DoneEditViewModelTests: XCTestCase {
         XCTAssertEqual(value, expectedValue)
     }
     
-    func test_doneButtonState을호출했을때_taskTitle과Category가값이있을때만True가반환되어야한다() {
+    func test_doneButtonState을호출했을때_taskTitle과Category가값이없다면False가반환되어야한다() {
         // given
         let expectation = XCTestExpectation(description: "true가 반환되어야 한다")
-        let expectedValue = true
-        var value = false
+        let expectedValue = false
+        var value = true
         
         // when
         _ = sut.doneButtonState
@@ -115,10 +104,10 @@ class DoneEditViewModelTests: XCTestCase {
         XCTAssertEqual(value, expectedValue)
     }
     
-    func test_doneButtonTitle을호출했을때_수정하기_가반환되어야한다() {
+    func test_doneButtonTitle을호출했을때_추가하기_가반환되어야한다() {
         // given
-        let expectation = XCTestExpectation(description: "수정하기가반환되어야한다")
-        let expectedValue = "수정하기"
+        let expectation = XCTestExpectation(description: "추가하기가반환되어야한다")
+        let expectedValue = "추가하기"
         var value = ""
         
         // when
@@ -133,10 +122,10 @@ class DoneEditViewModelTests: XCTestCase {
         XCTAssertEqual(value, expectedValue)
     }
     
-    func test_viewTitle을호출했을때_한_일_수정하기_가반환되어야한다() {
+    func test_viewTitle을호출했을때_한_일_추가하기_가반환되어야한다() {
         // given
-        let expectation = XCTestExpectation(description: "한 일 수정하기가반환되어야한다")
-        let expectedValue = "한 일 수정하기"
+        let expectation = XCTestExpectation(description: "한 일 추가하기가반환되어야한다")
+        let expectedValue = "한 일 추가하기"
         var value = ""
         
         // when
